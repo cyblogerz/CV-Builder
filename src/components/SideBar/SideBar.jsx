e0637c0375e94a89f996e4d04ef58777966241e1
cyblogerz <pranavajay2002@gmail.com>
2023-09-23 13:08:16 +0530

    Updated buttons

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -6,14 +6,20 @@ import LayoutSection from './LayoutSection';
 const SideBar = () => {
   return (
     <div className=' w-full flex-1 flex-col mt-8 mr-6'>
-    <button class="px-4 py-2 font-semibold text-sm bg-cyan-500 text-white rounded-full shadow-lg">
+    <button class="px-4 py-2 font-semibold text-sm text-white 
+    bg-gradient-to-r from-blue-700 to-purple-800 text-white transform hover:scale-105 
+    transition-transform duration-300 
+    ease-in-out rounded-md shadow-md hover:shadow-lg focus:outline-none
+     ">
     <div className="flex flex-row items-center">
         <FaDownload />
         <p className='ml-2'>Download now</p>
     </div>
     
     </button>
-    <button class="px-4 py-2 font-semibold text-sm w-1/2 bg-cyan-500 ml-2 text-white rounded-full shadow-lg">
+    <button className="px-4 py-2 font-semibold text-sm w-1/2 
+    bg-gradient-to-r from-purple-800 to-indigo-800
+     ml-2 text-white rounded-md shadow-lg">
     <div className="flex flex-row items-center">
         <BsShare />
         <p className='ml-2'>Share</p>
